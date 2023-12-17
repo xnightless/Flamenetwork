5785316eab54d115d84c84a2700af0fac67ddac6
Shadow <149085044+Shadowproxynetwork@users.noreply.github.com>
2023-12-17 00:43:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="shortcut icon" id="dynamic-favicon" href="favicon.png">
+  <title id="dynamic-title">Classes</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="background-orange">
+    <div class="flame-network"></div>
+    <div class="search-bar">
+      <input type="text" id="searchbar" placeholder="Search">
+    </div>
+    <div class="nav-bar">
+      <!-- add your app links and icons here -->
+    </div>
+  </div>
+</body>
+</html>
