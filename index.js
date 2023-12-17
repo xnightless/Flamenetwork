0fbafb1b59026154fad8307aed7f55098c1c0aca
Shadow <149085044+Shadowproxynetwork@users.noreply.github.com>
2023-12-17 11:17:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <link rel="shortcut icon" id="dynamic-favicon" href="favicon.png">
-  <title id="dynamic-title">Classes</title>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-  <div class="background-orange">
-    <div class="flame-network"></div>
-    <div class="search-bar">
-      <input type="text" id="searchbar" placeholder="Search">
-    </div>
-    <div class="nav-bar">
-      <!-- add your app links and icons here -->
-    </div>
-  </div>
-</body>
-</html>
+import express from 'express';
+import http from 'node:http';
+import path from 'node:path';
+import cors from 'cors';
+import { createBareServer } from "@tomphttp/bare-server-node";
+import fetch from 'node-fetch';  // Make sure to install 'node-fetch' package
+const app = express();
+const server = http.createServer(app);
+const __dirname = process.cwd();
+const PORT = 8080;
+const bareServer = createBareServer('/outerspace/');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'static')));
+const routes = [
+  { path: '/', file: 'index.html' },
+  { path: '/news', file: 'apps.html' },
+  { path: '/events', file: 'games.html' },
+  { path: '/diagnostic', file: 'settings.html' },
+  { path: '/local-news', file: 'tabs.html' },
+  { path: '/image-galleries', file: 'go.html' },
+];
+app.get('/edu/*', cors({ origin: false }), async (req, res, next) => {
+  try {
+    const reqTarget = `https://raw.githubusercontent.com/InterstellarNetwork/Interstellar-Assets/main/${req.params[0]}`;
+    const asset = await fetch(reqTarget);
+    
+    if (asset.ok) {
+      const data = await asset.arrayBuffer();
+      res.end(Buffer.from(data));
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error('Error fetching:', error);
+    next(error);
+  }
+});
+routes.forEach((route) => {
+  app.get(route.path, (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', route.file));
+  });
+});
+server.on('request', (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+server.on('upgrade', (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+server.listen(PORT, () => {
+  console.log(`Running at http://localhost:${PORT}`);
+});
