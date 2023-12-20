2fd601a6916adc4e029a63686750a0744487ec2a
Shadow <149085044+Shadowproxynetwork@users.noreply.github.com>
2023-12-20 11:51:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,59 @@
-import express from 'express';
-import http from 'node:http';
-import path from 'node:path';
-import cors from 'cors';
-import { createBareServer } from "@tomphttp/bare-server-node";
-import fetch from 'node-fetch';  // Make sure to install 'node-fetch' package
-const app = express();
-const server = http.createServer(app);
-const __dirname = process.cwd();
-const PORT = 8080;
-const bareServer = createBareServer('/outerspace/');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'static')));
-const routes = [
-  { path: '/', file: 'index.html' },
-  { path: '/news', file: 'apps.html' },
-  { path: '/events', file: 'games.html' },
-  { path: '/diagnostic', file: 'settings.html' },
-  { path: '/local-news', file: 'tabs.html' },
-  { path: '/image-galleries', file: 'go.html' },
-];
-app.get('/edu/*', cors({ origin: false }), async (req, res, next) => {
-  try {
-    const reqTarget = `https://raw.githubusercontent.com/InterstellarNetwork/Interstellar-Assets/main/${req.params[0]}`;
-    const asset = await fetch(reqTarget);
-    
-    if (asset.ok) {
-      const data = await asset.arrayBuffer();
-      res.end(Buffer.from(data));
-    } else {
-      next();
-    }
-  } catch (error) {
-    console.error('Error fetching:', error);
-    next(error);
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  window.navigator.serviceWorker
+    .register("./sw.js", {
+      scope: __uv$config.prefix,
+    })
+    .then(() => {
+      let url = input.value.trim();
+      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+      else if (!(url.startsWith("https://") || url.startsWith("http://")))
+        url = "http://" + url;
+      sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+      location.href = "image-galleries";
+    });
+});
+
+
+function images(value) {
+    let iframe = document.querySelector(".iframe.active");
+    window.navigator.serviceWorker
+      .register("./sw.js", {
+        scope: __uv$config.prefix,
+      })
+      .then(() => {
+        let url = value.trim();
+        if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+        else if (!(url.startsWith("https://") || url.startsWith("http://")))
+          url = "https://" + url;
+        sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+        location.href = "/image-galleries";
+      });
   }
-});
-routes.forEach((route) => {
-  app.get(route.path, (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', route.file));
-  });
-});
-server.on('request', (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
-  } else {
-    app(req, res);
+  
+  function blank(value) {
+    let iframe = document.querySelector(".iframe.active");
+    window.navigator.serviceWorker
+      .register("./sw.js", {
+        scope: __uv$config.prefix,
+      })
+      .then(() => {
+        let url = value.trim();
+        if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+        else if (!(url.startsWith("https://") || url.startsWith("http://")))
+          url = "https://" + url;
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+      });
   }
-});
-server.on('upgrade', (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
+  
+  function isUrl(val = "") {
+    if (
+      /^http(s?):\/\//.test(val) ||
+      (val.includes(".") && val.substr(0, 1) !== " ")
+    )
+      return true;
+    return false;
   }
-});
-server.listen(PORT, () => {
-  console.log(`Running at http://localhost:${PORT}`);
-});
